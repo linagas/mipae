0bc18e055dadd29a9f98536def6760263c457be1
Leila <leiladip33@gmail.com>
2015-10-04 09:29:18 -0430

    commit

--- application/views/backend/footer.php
+++ application/views/backend/footer.php
@@ -7,16 +7,11 @@
 
 
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="<?php echo base_url();?>assets/js/ie10-viewport-bug-workaround.js"></script>
-     <script type="text/javascript">
-        var proyectURL = "<?php echo base_url();?>";
-    </script>
     <script src="<?php echo base_url();?>assets/js/jquery-1.11.2.js" type="text/javascript"></script>
+    <script src="<?php echo base_url();?>assets/js/jquery.validate.min.js" type="text/javascript"></script>
     <script src="<?php echo base_url();?>assets/js/ie10-viewport-bug-workaround.js"></script>
-    
-        <!-- jQuery -->
-    <script src="<?php echo base_url(); ?>assets/js/jquery.js"></script>
-
+     <script src="<?php echo base_url();?>assets/js/generic.js"></script>
+      <script src="<?php echo base_url();?>assets/js/scripts.js"></script>
     <!-- Bootstrap Core JavaScript -->
     <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
 
@@ -25,6 +20,12 @@
 
     <!-- Custom Theme JavaScript -->
     <script src="<?php echo base_url(); ?>assets/js/sb-admin-2.js"></script>
-     <script src="<?php echo base_url();?>assets/js/generic.js"></script>
+    <script> 
+        var projectURL	='<?= base_url()?>';
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+    </script>
+
   </body>
 </html>
--- application/views/backend/rol/lista_roles.php
+++ application/views/backend/rol/lista_roles.php
@@ -4,89 +4,100 @@
         <li class="active"><a href="<?php echo base_url(); ?>usuarios/get_usuarios">Lista de Roles </a></li>
     </ol>  
 </div>  
+  
 <div class="row">  
-    <div class="col-md-1"></div>  
-    <div class="col-md-10">
-        <div class="row">  
-            <ol class="breadcrumb panel-info">  
-                <li><a href="#">Rol</a></li>  
-                <li class="active"><a href="#">Agregar nuevo rol</a></li>  
-            </ol>  
-        </div>  
-        <div class="row">  
-            <div class="col-md-1">  
-            </div>
-            <div class="col-md-10">   
-                <div class="col-xs-12 col-sm-6 col-md-12">  
-                    <h4>Añadir rol</h4>  
-                </div>  
-                <div class="col-xs-12 col-sm-6 col-md-12">  
-                    <p>  
-                    <br/>  
-                    </p>  
-                </div>  
-                <div class="box-content">  
-                    <div class="col-xs-12">  
-                    <?php echo form_open(site_url("rol/insert_rol"))?>  
-                    <?php echo form_hidden("process",  TRUE);?>  
-                       <div class="row">  
-                            <div class="col-lg-6">  
-                                <div class="input-group">  
-                                    <span class="input-group-addon">Rol:</span>  
-                                    <input name="nombre" class="form-control" type="text" required="required" placeholder="Administrador" pattern="[a-zA-Z ]*");?>  
-                                </div><!-- /input-group -->  
-                            </div>  
-                            <div class="col-lg-6">  
-                                <div class="input-group">  
-                                    <span class="btn btn-success"><span class="glyphicon glyphicon-refresh fa-1x"></span></span>
-                                </div><!-- /input-group -->  
-                            </div>  
-                       </div>  
-                    </div>
-               <?php echo form_close();?>
-               </div>
-        <div class="row">
-            <div class="col-xs-12 col-sm-6 col-md-8">
-                <h4>Roles de Usuarios</h4><small>Lista de Roles</small>
-            </div>
-            <div class="col-xs-6 col-md-4">
-                <button id="crear_rol" type="button" class="btn btn-info">Nuevo Rol</button>
-            </div> 
-        </div>
-        <div class="row">
-            
-        </div>
-        <div class="row">
-                <div class="table-responsive">
-                        <?php if(empty($roles)){ ?>
-                        {roles}
-                        <div class="alert alert-warning" role="alert">
-                         Actualmente no exiten roles disponibles 
-                        </div>   
-                       <?php }else{ ?>
-                        <table class="table table-hover">
-                        <thead>
-                            <tr>
-                                <th>Rol</th>
-                                <th colspan="3">Opciones</th>
-                            <tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td> <h4> {rol} </h4> </td>
-                                <td>
-                                <a href="<?php echo site_url('usuarios/update_usuario/{id_usuario}')?>"><span class="glyphicon glyphicon-refresh fa-2x">Editar</span> </a>
-                                <a href="<?php echo site_url('usuarios/detalle_usuario/{id_usuario}')?>"><span class="glyphicon glyphicon-list-alt fa-2x">Detalle</span> </a>
-                                <a href="<?php echo site_url('usuarios/delete_usuario/{id_usuario}')?>"><span class="fa fa-trash-o fa-2x">Eliminar</span> </a>   
-                                </td>
-                            </tr>
-                        </tbody>
-                        </table>
-                        {/roles}
-                        <?php } ?>
-                        <?php echo form_close(); ?>
-                </div>
-            </div>
-    </div>
     <div class="col-md-1"></div>
+    <div class="col-md-10">   
+        <div class="col-xs-12 col-sm-6 col-md-12">  
+            <h4>Añadir rol</h4>  
+        </div>  
+        <div class="col-xs-12 col-sm-6 col-md-12">  
+        </div>  
+        <div class="box-content">  
+            <div class="col-xs-12">
+                <form id="insertar-rol-form" role="form">
+                    <?php echo form_hidden("process",  TRUE);?>  
+                    <div class="row">  
+                         <div class="col-lg-6">  
+                             <div class="input-group">  
+                                 <span class="input-group-addon">Rol:</span>  
+                                 <input id="nombre_rol" name="nombre" class="form-control" type="text"  placeholder="Administrador" pattern="[a-zA-Z ]*" required="required">  
+                             </div><!-- /input-group -->  
+                         </div>  
+                         <div class="col-lg-6">  
+                             <div class="input-group">  
+                                 <button id="btn-crear-rol" type="button" class="btn btn-info">
+                                     <span class="glyphicon glyphicon-plus fa-1x"></span>
+                                 </button>
+                             </div><!-- /input-group -->  
+                         </div>  
+                    </div>  
+                </form>
+            </div>
+       </div>
+<div class="row">
+    <div class="col-xs-12 col-sm-6 col-md-8">
+        <br>
+        <h4>Roles de Usuarios</h4><small>Lista de Roles</small>
+    </div>
+    <div class="col-xs-6 col-md-4">
+        <div id="notifications-rol"></div>
+        
+        
+    </div> 
+</div>
+<div class="row">
+
+</div>
+<div class="row">
+        <div class="table-responsive">
+                <?php if(empty($lista_roles)){ ?>
+                <div class="alert alert-warning" role="alert">
+                 Actualmente no exiten roles disponibles 
+                </div>   
+               <?php }else{ ?>
+                <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Rol</th>
+                        <th
+                            >Opciones</th>
+                    <tr>
+                </thead>
+                <tbody>
+                    {lista_roles}
+                    <tr>
+                        <td> <h4> {rol} </h4> </td>
+                        <td>
+                        <div>
+                        <a href="<?php echo site_url('usuarios/update_usuario/{id}')?>">
+                            <span class="btn btn-info" data-toggle="tooltip" data-placement="left" title="Editar">
+                                <i class="glyphicon glyphicon-edit fa-1x"></i>
+                            </span>
+                        </a>
+                        <a href="<?php echo site_url('usuarios/detalle_usuario/{id}')?>">
+                            <span class="btn btn-success" data-toggle="tooltip" data-placement="left" title="Detalles">
+                                <i class="glyphicon glyphicon-list-alt fa-1x"></i>
+                            </span>
+                        </a>
+                            
+                            <div class="col-md-2">
+                                <div>
+                                <a class="btn-eliminar-rol" data-link="<?php echo site_url('rol/delete_rol/{id} ')?>">
+                                <span class="btn btn-danger " data-toggle="tooltip" data-placement="left" title="Eliminar rol">
+                                    <i class="glyphicon glyphicon-trash fa-1x"></i>
+                                </span>
+                                </a>
+                                </div>
+                            </div>
+                        </div>
+                        </td>
+                    </tr>
+                    {/lista_roles}
+                </tbody>
+                </table>
+                <?php } ?>
+                <?php echo form_close(); ?>
+        </div>
+    </div>
 </div>
